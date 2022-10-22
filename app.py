6ed9d8f9c2fd2c06542dc91d0459c2cbc93263e1
msayyou <84775525+msayyou@users.noreply.github.com>
2022-10-23 00:09:21 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import pickle
+
+import pandas as pd
+from flask import Flask, request
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@flask_app.route("/prevision", methods=["POST"])
+def predict():
+    json_ = request.json
+    query_df = pd.DataFrame(json_)
+    prevision = model.predict(query_df)
+    return f"The prediction for this individual is {round(prevision[0], 2)}!"
+
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
